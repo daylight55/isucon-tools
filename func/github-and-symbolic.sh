7c266c3ad94f705a0321c48eec17bf2c673fce19
daylight55 <jojomasanori3@gmail.com>
2022-07-21 23:44:27 +0900

    complet: setup

--- func/github-and-symbolic.sh
+++ func/github-and-symbolic.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -xe
+set -e
+
+#TODO: ディレクトリチェック処理入れる
 
 GIT_REMOTE_REPOSITORY="${1}"
 APP_NAME="${2}"
+APP_DIR="${3}"
 
 echo "<==== STOP SYSTEMD UNIT      ====>"
 sudo systemctl disable --now apparmor
@@ -11,18 +14,19 @@ sudo systemctl enable --now ${APP_NAME}.ruby
 
 # webappディレクトリにetcコンフィグを持ってくる
 echo "<==== COPY ETC FILES     ====>"
-cd /home/isucon/webapp
+WEB_APP_DIR=/home/isucon/${APP_DIR}
+cd ${WEB_APP_DIR}
 mkdir etc
-sudo cp /etc/nginx/nginx.conf /home/isucon/webapp/etc/nginx.conf
-sudo cp /etc/mysql/my.cnf /home/isucon/webapp/etc/my.cnf
-sudo cp /etc/systemd/system/${APP_NAME}.ruby.service /home/isucon/webapp/etc/${APP_NAME}.ruby.service
+sudo cp /etc/nginx/nginx.conf ${WEB_APP_DIR}/etc/nginx.conf
+sudo cp /etc/mysql/my.cnf ${WEB_APP_DIR}/etc/my.cnf
+sudo cp /etc/systemd/system/${APP_NAME}.ruby.service ${WEB_APP_DIR}/etc/${APP_NAME}.ruby.service
 
 echo "<==== SYMBOLIC LINK     ====>"
 # etcにシンボリックリンクを貼る
-sudo ln -sb /home/isucon/webapp/etc/nginx.conf /etc/nginx/nginx.conf
+sudo ln -sb ${WEB_APP_DIR}/etc/nginx.conf /etc/nginx/nginx.conf
 # NOTICE: my.cnfのシンボリックリンクは/etc/mysqlの場合読まれないので/etc直下に置く
-sudo ln -sb /home/isucon/webapp/etc/my.cnf /etc/my.cnf
-sudo ln -sb /home/isucon/webapp/etc/${APP_NAME}.ruby.service /etc/systemd/system/${APP_NAME}.ruby.service
+sudo ln -sb ${WEB_APP_DIR}/etc/my.cnf /etc/my.cnf
+sudo ln -sb ${WEB_APP_DIR}/etc/${APP_NAME}.ruby.service /etc/systemd/system/${APP_NAME}.ruby.service
 
 # git config~push
 echo "<==== GIT SETTING     ====>"
--- setup.sh
+++ setup.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 source ./.env
 
-ISUCON_1_HOST=${1:-isu10-1}
-ISUCON_2_HOST=${2:-isu10-2}
-ISUCON_3_HOST=${3:-isu10-3}
-
-for remote in ${ISUCON_1_HOST}; do
+for remote in ${ISUCON_1_HOST} ${ISUCON_2_HOST} ${ISUCON_3_HOST}; do
   echo "Deploying to ${remote}"
   ssh ${remote} "bash -s" < ./func/install.sh
   ./func/send.sh ${remote}
-  ssh ${remote} "bash -s" < ./func/github-and-symbolic.sh ${GIT_REMOTE_REPOSITORY} ${APP_NAME}
+  ssh ${remote} "bash -s" < ./func/github-and-symbolic.sh ${GIT_REMOTE_REPOSITORY} ${APP_NAME} ${APP_DIR}
 done
 
 ssh ${ISUCON_1_HOST} "bash -s" <<EOF
 echo "<==== GIT COMMIT ${GIT_REMOTE_REPOSITORY}   ====>"
+cd ${APP_DIR}
 git add .
 git commit -m "first commit"
 git push -u origin master
@@ -21,12 +18,14 @@ EOF
 
 ssh ${ISUCON_2_HOST} "bash -s" <<EOF
 echo "<==== ${ISUCON_2_HOST} GIT RESET HARD  ====>"
+cd ${APP_DIR}
 git fetch
 git reset --hard origin/master
 EOF
 
 ssh ${ISUCON_3_HOST} "bash -s" <<EOF
 echo "<==== ${ISUCON_3_HOST} GIT RESET HARD  ====>"
+cd ${APP_DIR}
 git fetch
 git reset --hard origin/master
 EOF
